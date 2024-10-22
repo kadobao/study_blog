6223ffca5e2d7b811601376fecd3ba8a9589527d
cf <toysouth0@gmail.com>
2024-10-23 02:27:10 +0800

    修改了图标

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -15,7 +15,7 @@ export default hopeTheme({
 
   logo: "https://theme-hope-assets.vuejs.press/logo.svg",
 
-  repo: "vuepress-theme-hope/vuepress-theme-hope",
+  repo: "kadobao",
 
   docsDir: "src",
 
