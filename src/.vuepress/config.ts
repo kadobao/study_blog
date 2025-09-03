c75a57e5927b0f46df08b99e22d92fc21fa78655
XJ <XJ@qq.com>
2025-09-03 13:18:00 +0800

    修改端口

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -9,6 +9,8 @@ import theme from "./theme.js";
 export default defineUserConfig({
   base: process.env.NODE_ENV === 'production' ? "/study_blog/" : "/",
 
+  port: 4343,
+
   lang: "zh-CN",
   title: "个人学习记录博客",
   description: "vuepress-theme-hope 的博客演示",
