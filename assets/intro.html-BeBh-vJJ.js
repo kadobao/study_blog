80a1a7ab8fe6eecbef4bc99e814a09195a23b87d
kadobao <toysouth0@gmail.com>
2025-12-11 06:47:35 +0000

    Deploying to gh-pages from @ kadobao/study_blog@6e67a7924a3badb902d34c807cca7c29224afc34 🚀

---
+++ assets/intro.html-BeBh-vJJ.js
@@ -0,0 +1,10 @@
+import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as t,a as o,o as n}from"./app-H_NJr6rj.js";const r={};function a(i,c){return n(),t("div",null,[o(` ---
+# icon: circle-info
+# cover: /assets/images/cover3.jpg
+index: false
+article: false
+---
+
+# 介绍页
+
+将你的个人介绍和档案放置在此处。 `)])}const p=e(r,[["render",a]]),l=JSON.parse('{"path":"/intro.html","title":"","lang":"zh-CN","frontmatter":{"head":[["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2025-10-31T06:11:21.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"XJ\\",\\"url\\":\\"https://kadobao.github.io/study_blog/acrticle/\\"}]}"],["meta",{"property":"og:url","content":"https://mister-hope.github.io/intro.html"}],["meta",{"property":"og:site_name","content":"个人学习记录博客"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2025-10-31T06:11:21.000Z"}],["meta",{"property":"article:modified_time","content":"2025-10-31T06:11:21.000Z"}]]},"git":{"createdTime":1724365063000,"updatedTime":1761891081000,"contributors":[{"name":"cf","username":"cf","email":"toysouth0@gmail.com","commits":1,"url":"https://github.com/cf"},{"name":"XJ","username":"XJ","email":"XJ@qq.com","commits":1,"url":"https://github.com/XJ"}]},"readingTime":{"minutes":0.09,"words":27},"filePathRelative":"intro.md","excerpt":"<!-- ---\\n# icon: circle-info\\n# cover: /assets/images/cover3.jpg\\nindex: false\\narticle: false\\n---\\n\\n# 介绍页\\n\\n将你的个人介绍和档案放置在此处。 -->\\n"}');export{p as comp,l as data};
