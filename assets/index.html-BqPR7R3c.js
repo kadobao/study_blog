cf58a43167b964b24d4317e573c19e6548dcdf23
kadobao <toysouth0@gmail.com>
2026-06-16 07:12:20 +0000

    Deploying to gh-pages from @ kadobao/study_blog@095d7597991c3daecfe6d270e42a0da9272f4e49 🚀

---
+++ assets/index.html-BqPR7R3c.js
@@ -0,0 +1,14 @@
+import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as o,e as n,a,r,o as c}from"./app-C8R_mHrE.js";const i={};function m(p,s){const t=r("Catalog");return c(),o("div",null,[n(t),a(` # MES 开发到上线、维护
+
+## 职责说明
+
+- **代码开发**：负责 MES 相关功能的代码编写与维护
+- **项目上线**：推动 MES 系统顺利部署至生产环境
+- **设备验收**：测试设备数据采集功能是否正常
+- **旧设备改造**：将旧设备改造成可接入 MES 系统的形式（添加串口模块，经以太网接入MES系统，使用modbus tcp进行读取）
+- **培训支持**：为班组长提供系统操作培训
+- **采购选型**：参与新设备的选型与采购工作
+- **现场调试**：设备与系统的现场安装调试
+- **问题排查**：处理现场出现的各类技术问题
+- **文档编写**：编写技术手册、操作 SOP 及使用规范
+- **技术支持**：回复现场班组长遇到的问题与疑问 `)])}const g=e(i,[["render",m]]),u=JSON.parse('{"path":"/acrticle/MES/","title":"MES","lang":"zh-CN","frontmatter":{"title":"MES","index":false,"icon":"industry","category":["MES"],"head":[["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"MES\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2026-06-16T07:10:41.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"XJ\\",\\"url\\":\\"https://kadobao.github.io/study_blog/acrticle/\\"}]}"],["meta",{"property":"og:url","content":"https://mister-hope.github.io/study_blog/acrticle/MES/"}],["meta",{"property":"og:site_name","content":"个人学习记录博客"}],["meta",{"property":"og:title","content":"MES"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2026-06-16T07:10:41.000Z"}],["meta",{"property":"article:modified_time","content":"2026-06-16T07:10:41.000Z"}]]},"git":{"createdTime":1781591645000,"updatedTime":1781593841000,"contributors":[{"name":"XJ","username":"XJ","email":"XJ@qq.com","commits":4,"url":"https://github.com/XJ"}]},"readingTime":{"minutes":0.75,"words":224},"filePathRelative":"acrticle/MES/README.md","excerpt":""}');export{g as comp,u as data};
